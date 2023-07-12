761c10e7a024a4a2c3e97ef2732424b1f979ced5
leonelvalentinn <leonelvalentinn@gmail.com>
2023-07-12 12:29:43 -0600

    Edit promos

--- js/modales.js
+++ js/modales.js
@@ -3,7 +3,7 @@ const infoModal = [
     id: 1,
     title: "Hotel Rosita",
     img: "imagenes/promo-hotel-rosita.jpeg",
-    description: "Obten este beneficio solo con la membresia Multiservice Card",
+    description: "Obtén este beneficio solo con la membresía Multiservice Card",
     direccion: " Paseo Díaz Ordaz 901, Centro, 48300 Puerto Vallarta, Jal.",
     mapa: " <iframe class='iframe-modal' src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3734.344397206202!2d-105.23540612602231!3d20.61481440186987!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x84214545188c68dd%3A0x355eb53587ca7d85!2sHotel%20Rosita!5e0!3m2!1ses-419!2smx!4v1681323222972!5m2!1ses-419!2smx' style='border:0;'  loading='lazy' referrerpolicy='no-referrer-when-downgrade'></iframe>",
     contacto: "tel:3221761111",
@@ -12,7 +12,7 @@ const infoModal = [
   {
     id: 2,
     title: "Publicity Bus",
-    description: "Obten este beneficio solo con la membresia Multiservice Card",
+    description: "Obtén este beneficio solo con la membresía Multiservice Card",
     direccion:
       "C. Prol. Brasil 1404-planta baja, 5 de Diciembre, 48350 Puerto Vallarta, Jal.",
     img: "imagenes/promo-publicity-bus.jpeg",
@@ -24,7 +24,7 @@ const infoModal = [
     id: 3,
     title: "Hotel Pescador",
     img: "imagenes/promo-hotel-pescador.jpeg",
-    description: "Obten este beneficio solo con la membresia Multiservice Card",
+    description: "Obtén este beneficio solo con la membresía Multiservice Card",
     direccion: "Paraguay 1117, Centro, 48350 Puerto Vallarta, Jal.",
     mapa: " <iframe class='iframe-modal' src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3734.290561368951!2d-105.23461222602229!3d20.61701010179575!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x84214544ba5da62d%3A0x2b6991562d8889c0!2sHotel%20El%20Pescador!5e0!3m2!1ses-419!2smx!4v1681326131625!5m2!1ses-419!2smx' style='border:0;'  loading='lazy' referrerpolicy='no-referrer-when-downgrade'></iframe>",
     contacto: "tel:3221761100",
@@ -33,7 +33,7 @@ const infoModal = [
   {
     id: 4,
     title: "Clínica Juarez",
-    description: "Obten este beneficio solo con la membresia Multiservice Card",
+    description: "Obtén este beneficio solo con la membresía Multiservice Card",
     direccion:
       "Basilio Badillo 317, Zona Romántica, Emiliano Zapata, 48380 Puerto Vallarta, Jal.",
     img: "imagenes/promo-juarez.jpg",
@@ -44,7 +44,7 @@ const infoModal = [
   {
     id: 5,
     title: "Pro Garage",
-    description: "Obten este beneficio solo con la membresia Multiservice Card",
+    description: "Obtén este beneficio solo con la membresía Multiservice Card",
     direccion: "C. Ecuador 1426, 5 de Diciembre, 48350 Puerto Vallarta, Jal.",
     img: "imagenes/promo-cardwash.jpg",
     mapa: " <iframe class='iframe-modal' src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14936.692566324813!2d-105.24596288677388!3d20.62179830369107!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x842145972f5b2c15%3A0xc796a2327cdf7621!2sPro%20Garage!5e0!3m2!1ses-419!2smx!4v1681326401185!5m2!1ses-419!2smx' style='border:0;'  loading='lazy' referrerpolicy='no-referrer-when-downgrade'></iframe>",
@@ -54,7 +54,7 @@ const infoModal = [
   {
     id: 6,
     title: "Makeup Kenia",
-    description: "Obten este beneficio solo con la membresia Multiservice Card",
+    description: "Obtén este beneficio solo con la membresía Multiservice Card",
     direccion: "C. Juárez 661, Centro, 48300 Puerto Vallarta, Jal.",
     img: "imagenes/promo-kenia.jpg",
     mapa: "<iframe class='iframe-modal' src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3734.4226463978966!2d-105.23571372602235!3d20.611622601977505!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8421455de49c0455%3A0x79de11559e295ba0!2sCl%C3%ADnica%20Dental%20Ju%C3%A1rez!5e0!3m2!1ses-419!2smx!4v1681326617297!5m2!1ses-419!2smx' style='border:0;'  loading='lazy' referrerpolicy='no-referrer-when-downgrade'></iframe>",
@@ -65,7 +65,7 @@ const infoModal = [
     id: 7,
     title: "Laptop Repair",
     img: "imagenes/promo-pc.jpg",
-    description: "Obten este beneficio solo con la membresia Multiservice Card",
+    description: "Obtén este beneficio solo con la membresía Multiservice Card",
     direccion: "C. Revolucion 409A, Coapinole, 48290 Puerto Vallarta, Jal.",
     mapa: " <iframe class='iframe-modal' src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3733.419671747123!2d-105.2147065260212!3d20.6524984005979!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x84214f882e7da7f7%3A0xc04b02153cf35985!2sLaptop%20Repair%20-%20Reparacion%20de%20computadoras%20y%20celulares!5e0!3m2!1ses-419!2smx!4v1681326774268!5m2!1ses-419!2smx' style='border:0;'  loading='lazy' referrerpolicy='no-referrer-when-downgrade'></iframe>",
     contacto: "https://api.whatsapp.com/send/?phone=523221396110",
@@ -74,7 +74,7 @@ const infoModal = [
   {
     id: 8,
     title: "Cosmo Optico",
-    description: "Obten este beneficio solo con la membresia Multiservice Card",
+    description: "Obtén este beneficio solo con la membresía Multiservice Card",
     direccion: "No hay dirección fisica",
     img: "imagenes/promo-lentes.jpg",
     mapa: " <p>Lo sentimos no hay dirección fisica  para este negocio, pero puedes contactarnos mediante el boton de abajo<p/>",
@@ -84,7 +84,7 @@ const infoModal = [
   {
     id: 9,
     title: "Video Vigilancia ",
-    description: "Obten este beneficio solo con la membresia Multiservice Card",
+    description: "Obtén este beneficio solo con la membresía Multiservice Card",
     direccion: "No hay dirección fisica",
     img: "imagenes/promo-vigilancia.jpg",
     mapa: " <p>Lo sentimos no hay dirección fisica  para este negocio, pero puedes contactarnos mediante el boton de abajo<p/>",
@@ -94,7 +94,7 @@ const infoModal = [
   {
     id: 10,
     title: "Jennifer Serrano",
-    description: "Obten este beneficio solo con la membresia Multiservice Card",
+    description: "Obtén este beneficio solo con la membresía Multiservice Card",
     direccion: "C. Nicolás Régules 61, Mexicaltzingo, 44100 Guadalajara, Jal.",
     img: "imagenes/promo-casas.jpg",
     mapa: " <iframe class='iframe-modal' src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3733.075130659198!2d-103.35276619117518!3d20.666522180814013!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8428b1f888992a13%3A0x7097911930f73230!2sGran%20Casa%20Xalisco!5e0!3m2!1ses-419!2smx!4v1681328385308!5m2!1ses-419!2smx' style='border:0;'  loading='lazy' referrerpolicy='no-referrer-when-downgrade'></iframe>",
@@ -104,7 +104,7 @@ const infoModal = [
   {
     id: 11,
     title: "EndoCenter Rivera",
-    description: "Obten este beneficio solo con la membresia Multiservice Card",
+    description: "Obtén este beneficio solo con la membresía Multiservice Card",
     direccion:
       "Blvd. Riviera Nayarit #2, interior 6, entre Av. Paseo de la Costa y Av. El Roble, Nuevo Vallarta, Nayarit. Plaza el Roble. 63735 Nuevo Vallarta, México",
     img: "imagenes/promo-endo.jpg",
@@ -114,7 +114,7 @@ const infoModal = [
   {
     id: 12,
     title: "Imagine by Feat Nutrición",
-    description: "Obten este beneficio solo con la membresia Multiservice Card",
+    description: "Obtén este beneficio solo con la membresía Multiservice Card",
     direccion: "Francia 203, Versalles, 48310 Puerto Vallarta, Jal.",
     img: "imagenes/promo-images.jpg",
     mapa: " <iframe class='iframe-modal' src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3733.7931132574267!2d-105.23014782551459!3d20.6372880011106!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8421457b18057241%3A0xef618a49d20390f5!2sFrancia%20203%2C%20Versalles%2C%2048310%20Puerto%20Vallarta%2C%20Jal.!5e0!3m2!1ses-419!2smx!4v1683826235897!5m2!1ses-419!2smx' style='border:0;'  loading='lazy' referrerpolicy='no-referrer-when-downgrade'></iframe>",
@@ -124,7 +124,7 @@ const infoModal = [
   {
     id: 13,
     title: "Servicio Rodriguez",
-    description: "Obten este beneficio solo con la membresia Multiservice Card",
+    description: "Obtén este beneficio solo con la membresía Multiservice Card",
     direccion: "No hay dirección fisica",
     img: "imagenes/promo-acondi.jpg",
     mapa: "<p>Lo sentimos no hay dirección fisica para este negocio, pero puedes contactarnos mediante el boton de abajo<p/>",
@@ -134,7 +134,7 @@ const infoModal = [
   {
     id: 14,
     title: "Gelato",
-    description: "Obten este beneficio solo con la membresia Multiservice Card",
+    description: "Obtén este beneficio solo con la membresía Multiservice Card",
     direccion:
       "Gelato Mx7, Berna 231-4, Versalles, 48310 Puerto Vallarta, Jal.",
     img: "imagenes/promo-helados.jpg",
@@ -145,7 +145,7 @@ const infoModal = [
   {
     id: 15,
     title: "Mare Estudio Creativo",
-    description: "Obten este beneficio solo con la membresia Multiservice Card",
+    description: "Obtén este beneficio solo con la membresía Multiservice Card",
     direccion: "No hay dirección fisica",
     img: "imagenes/promo-foto.jpeg",
     mapa: "<p>Lo sentimos no hay dirección fisica para este negocio, pero puedes contactarnos mediante el boton de abajo<p/>",
@@ -154,7 +154,7 @@ const infoModal = [
   {
     id: 16,
     title: "Joyeria Luna",
-    description: "Obten este beneficio solo con la membresia Multiservice Card",
+    description: "Obtén este beneficio solo con la membresía Multiservice Card",
     direccion:
       "Avenida Los Tules #178 Int Isla K1, Plaza Caracol, Puerto Vallarta, Mexico",
     img: "imagenes/promo-luna.jpeg",
@@ -164,7 +164,7 @@ const infoModal = [
   {
     id: 17,
     title: "Manuel Sanchez Gestorías Vehiculares",
-    description: "Obten este beneficio solo con la membresia Multiservice Card",
+    description: "Obtén este beneficio solo con la membresía Multiservice Card",
     direccion: "No hay dirección fisica",
     img: "imagenes/promo-sanchez.png",
     mapa: "<p>Lo sentimos no hay dirección fisica para este negocio, pero puedes contactarnos mediante el boton de abajo<p/>",
@@ -173,7 +173,7 @@ const infoModal = [
   {
     id: 18,
     title: "Quinta Paloma Campestre",
-    description: "Obten este beneficio solo con la membresia Multiservice Card",
+    description: "Obtén este beneficio solo con la membresía Multiservice Card",
     direccion:
       "C. 16 de Septiembre 283, Rancho Alegre, 48290 Puerto Vallarta, Jal.",
     img: "imagenes/promo-paloma.png",
@@ -183,11 +183,38 @@ const infoModal = [
   {
     id: 19,
     title: "Clínica dental Juarez",
-    description: "Obten este beneficio solo con la membresia Multiservice Card",
+    description: "Obtén este beneficio solo con la membresía Multiservice Card",
     direccion: "C. Juárez 661, Centro, 48300 Puerto Vallarta, Jal.",
     img: "imagenes/promo-juarez.jpg",
     mapa: "<iframe class='iframe-modal' src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3734.4226463978966!2d-105.23571372602235!3d20.611622601977505!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8421455de49c0455%3A0x79de11559e295ba0!2sCl%C3%ADnica%20Dental%20Ju%C3%A1rez!5e0!3m2!1ses-419!2smx!4v1681326617297!5m2!1ses-419!2smx' style='border:0;'  loading='lazy' referrerpolicy='no-referrer-when-downgrade'></iframe>",
     contacto: "https://api.whatsapp.com/send/?phone=523221121616",
+  },
+  {
+    id: 20,
+    title: "Spa Seattle",
+    description: "Obtén este beneficio solo con la membresía Multiservice Card",
+    direccion: "C. Popa local 32-B, Marina del rey, 48335 Puerto Vallarta, Jal.",
+    img: "imagenes/seatle-spa.jpeg",
+    mapa: "<iframe src='https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14932.277654819396!2d-105.2495426!3d20.6667548!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x842145e821467271%3A0x86ffd25c590fd837!2sSeattle%20Spa!5e0!3m2!1ses-419!2smx!4v1689182983213!5m2!1ses-419!2smx' style='border:0;' allowfullscreen='' loading='lazy' referrerpolicy='no-referrer-when-downgrade'></iframe>",
+    contacto: "https://api.whatsapp.com/send/?phone=3221311344",
+  },
+  {
+    id: 21,
+    title: "Carmen's Bliss Spa",
+    description: "Obtén este beneficio solo con la membresía Multiservice Card",
+    direccion: "C. Popa Local 03-B, Marina Vallarta, 48335 Puerto Vallarta, Jal.",
+    img: "imagenes/carmens-spa.jpeg",
+    mapa: "<iframe src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3733.0660884825384!2d-105.2493956!3d20.6668901!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x842145b8b6f66621%3A0x923b48db1f4cff5d!2sCarmen&#39;s%20Bliss%20Spa!5e0!3m2!1ses-419!2smx!4v1689183341752!5m2!1ses-419!2smx' style='border:0;' allowfullscreen='' loading='lazy' referrerpolicy='no-referrer-when-downgrade'></iframe>",
+    contacto: "tel:3222213132",
+  },
+  {
+    id: 22,
+    title: "JR Fishing Sailing Yatchs & Tours",
+    description: "Obtén este beneficio solo con la membresía Multiservice Card",
+    direccion: "Condominios Puesta del Sol, Local 11-D, Mariana Vallarta, Puerto Vallarta, Jal.",
+    img: "imagenes/yates-promo.jpg",
+    mapa: "<iframe src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3733.1425362984655!2d-105.25371249999999!3d20.663779299999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x842145b84b6daa39%3A0x3d1977fbf52983c8!2sClub%20de%20Tenis%20Puesta%20del%20Sol%20S.A.!5e0!3m2!1ses-419!2smx!4v1689184354344!5m2!1ses-419!2smx' style='border:0;' allowfullscreen='' loading='lazy' referrerpolicy='no-referrer-when-downgrade'></iframe>",
+    contacto: "https://api.whatsapp.com/send/?phone=3221087372",
   },
 ];
 
